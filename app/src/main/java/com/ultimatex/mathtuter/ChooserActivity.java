f6aac1184baaff49187f258b5511153f7d92b61e
lakinduakash <lakinduakash@gmail.com>
2018-04-11 00:05:03 +0530

    get MainActivity intent

--- app/src/main/java/com/ultimatex/mathtuter/ChooserActivity.java
+++ app/src/main/java/com/ultimatex/mathtuter/ChooserActivity.java
@@ -1,13 +1,25 @@
 package com.ultimatex.mathtuter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ChooserActivity extends AppCompatActivity {
+
+    private static final String INTENT_EXTRA_KEY =MainActivity.EXTRA_KEY_OP;
+    private String intentExtra;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooser);
+
+        Intent intent = getIntent();
+        intentExtra = intent.getStringExtra(INTENT_EXTRA_KEY);
+
+
     }
 }

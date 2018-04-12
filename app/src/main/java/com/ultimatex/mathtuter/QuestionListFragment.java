f624028b5cf3d642aa04eda2f7eec7dd463ff646
lakinduakash <lakinduakash@gmail.com>
2018-04-12 22:19:59 +0530

    new Fragment class

---
+++ app/src/main/java/com/ultimatex/mathtuter/QuestionListFragment.java
@@ -0,0 +1,86 @@
+package com.ultimatex.mathtuter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ultimatex.mathtuter.util.QuestionListAdapter;
+import com.ultimatex.mathtuter.util.RecyclerItemClickListener;
+
+public class QuestionListFragment extends Fragment {
+
+    RecyclerView rv;
+    RecyclerView.LayoutManager rvl;
+    RecyclerView.Adapter rva;
+
+    int a[] = new int[10000];
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public QuestionListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_question_list, container, false);
+
+        rv = (RecyclerView) view.findViewById(R.id.list_questions);
+        rv.setHasFixedSize(true);
+
+        rvl = new LinearLayoutManager(view.getContext());
+        rv.setLayoutManager(rvl);
+
+        rva = new QuestionListAdapter(a);
+        rv.setAdapter(rva);
+
+        RecyclerView.ItemDecoration ri = new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL);
+        rv.addItemDecoration(ri);
+
+        rv.addOnItemTouchListener(new RecyclerItemClickListener(view.getContext(), new RecyclerItemClickListener.OnClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                mListener.onFragmentInteraction();
+            }
+        }));
+
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction();
+    }
+}

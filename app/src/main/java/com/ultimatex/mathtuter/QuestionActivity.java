9d58b56145602a1c357ac6a567c4cf29e558610c
lakinduakash <lakinduakash@gmail.com>
2018-04-12 22:20:40 +0530

    Add fragment transactions

--- app/src/main/java/com/ultimatex/mathtuter/QuestionActivity.java
+++ app/src/main/java/com/ultimatex/mathtuter/QuestionActivity.java
@@ -1,13 +1,45 @@
 package com.ultimatex.mathtuter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
-public class QuestionActivity extends AppCompatActivity {
+public class QuestionActivity extends AppCompatActivity implements QuestionListFragment.OnFragmentInteractionListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        QuestionListFragment questionListFragment = new QuestionListFragment();
+
+        fragmentTransaction.add(R.id.fragment_container_question, questionListFragment).commit();
+
+
+    }
+
+    private void addFragment() {
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        QuestionFragment questionFragment = new QuestionFragment();
+
+        fragmentTransaction.replace(R.id.fragment_container_question, questionFragment).addToBackStack("b").commit();
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+        addFragment();
     }
 }

837adba98c1705cde570c4f9eb4ee885681e84e2
lakinduakash <lakinduakash@gmail.com>
2018-04-14 02:36:24 +0530

    Update Fragment

--- app/src/main/java/com/ultimatex/mathtuter/QuestionActivity.java
+++ app/src/main/java/com/ultimatex/mathtuter/QuestionActivity.java
@@ -1,5 +1,6 @@
 package com.ultimatex.mathtuter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -7,11 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 
 public class QuestionActivity extends AppCompatActivity implements QuestionListFragment.OnFragmentInteractionListener {
 
+    String intentExtraOP;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        intent = getIntent();
+        intentExtraOP = intent.getStringExtra(MainActivity.EXTRA_KEY_OP);
 
         FragmentManager fragmentManager;
         FragmentTransaction fragmentTransaction;
@@ -26,20 +32,21 @@ public class QuestionActivity extends AppCompatActivity implements QuestionListF
 
     }
 
-    private void addFragment() {
+    private void addQuestionFragment(int pos) {
         FragmentManager fragmentManager;
         FragmentTransaction fragmentTransaction;
 
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
 
-        QuestionFragment questionFragment = new QuestionFragment();
 
-        fragmentTransaction.replace(R.id.fragment_container_question, questionFragment).addToBackStack("b").commit();
+        QuestionFragment questionFragment = QuestionFragment.newInstance(intentExtraOP, pos);
+
+        fragmentTransaction.replace(R.id.fragment_container_question, questionFragment).addToBackStack(intentExtraOP).commit();
     }
 
     @Override
-    public void onFragmentInteraction() {
-        addFragment();
+    public void onFragmentInteraction(int position) {
+        addQuestionFragment(position);
     }
 }

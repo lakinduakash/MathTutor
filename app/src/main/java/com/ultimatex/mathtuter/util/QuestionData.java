ad41ed25a8a03f651be0bc713fd7d08b3ca38935
lakinduakash <lakinduakash@gmail.com>
2018-04-14 02:38:43 +0530

    first commit

---
+++ app/src/main/java/com/ultimatex/mathtuter/util/QuestionData.java
@@ -0,0 +1,14 @@
+package com.ultimatex.mathtuter.util;
+
+public class QuestionData {
+
+    private int id;
+
+    public QuestionData(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}

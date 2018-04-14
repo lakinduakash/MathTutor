18a9bb95538fcaa737246069b725b51e271ceffc
lakinduakash <lakinduakash@gmail.com>
2018-04-15 02:44:01 +0530

    add create queries

--- app/src/main/java/com/ultimatex/mathtuter/util/QuestionsDbContract.java
+++ app/src/main/java/com/ultimatex/mathtuter/util/QuestionsDbContract.java
@@ -2,21 +2,20 @@ package com.ultimatex.mathtuter.util;
 
 import android.provider.BaseColumns;
 
-public class QuestionsDbContract {
+public class QuestionsDbContract implements BaseColumns {
 
-    private static final String COLUMN_NAME_QUESTION = "question";
-    private static final String COLUMN_NAME_TYPE = "type";
-    private static final String COLUMN_NAME_OP1 = "op1";
-    private static final String COLUMN_NAME_OP2 = "op2";
-    private static final String COLUMN_NAME_OP3 = "op3";
-    private static final String COLUMN_NAME_OP4 = "op4";
-    private static final String COLUMN_NAME_ANSWER = "answer";
-    private static final String COLUMN_NAME_SOLVED = "solved";
+    public static final String COLUMN_NAME_QUESTION = "question";
+    public static final String COLUMN_NAME_TYPE = "type";
+    public static final String COLUMN_NAME_OP1 = "op1";
+    public static final String COLUMN_NAME_OP2 = "op2";
+    public static final String COLUMN_NAME_OP3 = "op3";
+    public static final String COLUMN_NAME_OP4 = "op4";
+    public static final String COLUMN_NAME_ANSWER = "answer";
+    public static final String COLUMN_NAME_SOLVED = "solved";
 
     private QuestionsDbContract() {
     }
 
-    ;
 
     public static class AdditionEntry implements BaseColumns {
         public static final String TABLE_NAME = "Addition";
@@ -29,6 +28,20 @@ public class QuestionsDbContract {
         public static final String COLUMN_NAME_OP4 = QuestionsDbContract.COLUMN_NAME_OP4;
         public static final String COLUMN_NAME_ANSWER = QuestionsDbContract.COLUMN_NAME_ANSWER;
         public static final String COLUMN_NAME_SOLVED = QuestionsDbContract.COLUMN_NAME_SOLVED;
+
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INT PRIMARY KEY," +
+                        COLUMN_NAME_QUESTION + " TEXT," +
+                        COLUMN_NAME_TYPE + " CHAR(10) NOT NULL, " +
+                        COLUMN_NAME_OP1 + " CHAR(10), " +
+                        COLUMN_NAME_OP2 + " CHAR(10), " +
+                        COLUMN_NAME_OP3 + " CHAR(10), " +
+                        COLUMN_NAME_OP4 + " CHAR(10), " +
+                        COLUMN_NAME_ANSWER + " CHAR(10), " +
+                        COLUMN_NAME_SOLVED + " CHAR(10)" +
+                        ")";
+
 
     }
 
@@ -44,6 +57,19 @@ public class QuestionsDbContract {
         public static final String COLUMN_NAME_ANSWER = QuestionsDbContract.COLUMN_NAME_ANSWER;
         public static final String COLUMN_NAME_SOLVED = QuestionsDbContract.COLUMN_NAME_SOLVED;
 
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INT PRIMARY KEY," +
+                        COLUMN_NAME_QUESTION + " TEXT," +
+                        COLUMN_NAME_TYPE + " CHAR(10) NOT NULL, " +
+                        COLUMN_NAME_OP1 + " CHAR(10), " +
+                        COLUMN_NAME_OP2 + " CHAR(10), " +
+                        COLUMN_NAME_OP3 + " CHAR(10), " +
+                        COLUMN_NAME_OP4 + " CHAR(10), " +
+                        COLUMN_NAME_ANSWER + " CHAR(10), " +
+                        COLUMN_NAME_SOLVED + " CHAR(10)" +
+                        ")";
+
     }
 
     public static class DivisionEntry implements BaseColumns {
@@ -58,10 +84,23 @@ public class QuestionsDbContract {
         public static final String COLUMN_NAME_ANSWER = QuestionsDbContract.COLUMN_NAME_ANSWER;
         public static final String COLUMN_NAME_SOLVED = QuestionsDbContract.COLUMN_NAME_SOLVED;
 
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INT PRIMARY KEY," +
+                        COLUMN_NAME_QUESTION + " TEXT," +
+                        COLUMN_NAME_TYPE + " CHAR(10) NOT NULL, " +
+                        COLUMN_NAME_OP1 + " CHAR(10), " +
+                        COLUMN_NAME_OP2 + " CHAR(10), " +
+                        COLUMN_NAME_OP3 + " CHAR(10), " +
+                        COLUMN_NAME_OP4 + " CHAR(10), " +
+                        COLUMN_NAME_ANSWER + " CHAR(10), " +
+                        COLUMN_NAME_SOLVED + " CHAR(10)" +
+                        ")";
+
     }
 
     public static class MultiplyEntry implements BaseColumns {
-        public static final String TABLE_NAME = "Multiply";
+        public static final String TABLE_NAME = "Multiplication";
 
         public static final String COLUMN_NAME_QUESTION = QuestionsDbContract.COLUMN_NAME_QUESTION;
         public static final String COLUMN_NAME_TYPE = QuestionsDbContract.COLUMN_NAME_TYPE;
@@ -71,6 +110,19 @@ public class QuestionsDbContract {
         public static final String COLUMN_NAME_OP4 = QuestionsDbContract.COLUMN_NAME_OP4;
         public static final String COLUMN_NAME_ANSWER = QuestionsDbContract.COLUMN_NAME_ANSWER;
         public static final String COLUMN_NAME_SOLVED = QuestionsDbContract.COLUMN_NAME_SOLVED;
+
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INT PRIMARY KEY," +
+                        COLUMN_NAME_QUESTION + " TEXT," +
+                        COLUMN_NAME_TYPE + " CHAR(10) NOT NULL, " +
+                        COLUMN_NAME_OP1 + " CHAR(10), " +
+                        COLUMN_NAME_OP2 + " CHAR(10), " +
+                        COLUMN_NAME_OP3 + " CHAR(10), " +
+                        COLUMN_NAME_OP4 + " CHAR(10), " +
+                        COLUMN_NAME_ANSWER + " CHAR(10), " +
+                        COLUMN_NAME_SOLVED + " CHAR(10)" +
+                        ")";
 
     }
 }

50ce2e0dbc49b0c50e3ece63f1345cf963418e13
lakinduakash <lakinduakash@gmail.com>
2018-04-15 02:46:07 +0530

    update variables

--- app/src/main/java/com/ultimatex/mathtuter/ChooserActivity.java
+++ app/src/main/java/com/ultimatex/mathtuter/ChooserActivity.java
@@ -1,15 +1,14 @@
 package com.ultimatex.mathtuter;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class ChooserActivity extends AppCompatActivity {
 
-    private static final String INTENT_EXTRA_KEY =MainActivity.EXTRA_KEY_OP;
+    private static final String INTENT_EXTRA_KEY = OperationArgs.EXTRA_KEY_OP;
     private String intentExtra;
 
     Button lessonButon;

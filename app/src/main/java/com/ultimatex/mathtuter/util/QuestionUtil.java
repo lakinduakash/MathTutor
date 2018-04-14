83b35fc567cd3cf6cdaa422cfdef63b4732a294f
lakinduakash <lakinduakash@gmail.com>
2018-04-15 02:44:36 +0530

    remove functions

--- app/src/main/java/com/ultimatex/mathtuter/util/QuestionUtil.java
+++ app/src/main/java/com/ultimatex/mathtuter/util/QuestionUtil.java
@@ -2,7 +2,7 @@ package com.ultimatex.mathtuter.util;
 
 import android.content.Context;
 
-import com.ultimatex.mathtuter.MainActivity;
+import com.ultimatex.mathtuter.OperationArgs;
 import com.ultimatex.mathtuter.tinydb.TinyDB;
 
 import java.util.ArrayList;
@@ -27,34 +27,35 @@ public class QuestionUtil {
     public static void init(Context context) {
         tinyDB = new TinyDB(context);
 
+
         Calendar calendar = Calendar.getInstance();
         boolean isOutdated = isOutDated();
 
         tinyDB.putInt(LAST_DATE, calendar.get(Calendar.DAY_OF_YEAR));
 
         if (tinyDB.getListInt(SP_ADD).isEmpty() || isOutdated) {
-            generateNewIds(MainActivity.EXTRA_ADD);
+            generateNewIds(OperationArgs.EXTRA_ADD);
             tinyDB.putListInt(SP_ADD, ID_ADD);
         } else {
             ID_ADD = tinyDB.getListInt(SP_ADD);
         }
 
         if (tinyDB.getListInt(SP_SUB).isEmpty() || isOutdated) {
-            generateNewIds(MainActivity.EXTRA_SUB);
+            generateNewIds(OperationArgs.EXTRA_SUB);
             tinyDB.putListInt(SP_SUB, ID_SUB);
         } else {
             ID_SUB = tinyDB.getListInt(SP_SUB);
         }
 
         if (tinyDB.getListInt(SP_MUL).isEmpty() || isOutdated) {
-            generateNewIds(MainActivity.EXTRA_MUL);
+            generateNewIds(OperationArgs.EXTRA_MUL);
             tinyDB.putListInt(SP_MUL, ID_MUL);
         } else {
             ID_MUL = tinyDB.getListInt(SP_MUL);
         }
 
         if (tinyDB.getListInt(SP_DIV).isEmpty() || isOutdated) {
-            generateNewIds(MainActivity.EXTRA_DIV);
+            generateNewIds(OperationArgs.EXTRA_DIV);
             tinyDB.putListInt(SP_DIV, ID_DIV);
         } else {
             ID_DIV = tinyDB.getListInt(SP_DIV);
@@ -70,7 +71,7 @@ public class QuestionUtil {
             ID_MUL = new ArrayList<Integer>();
             ID_DIV = new ArrayList<Integer>();
 
-            for (int i = 1000; i < 1100; i++) {
+            for (int i = 0; i < 10; i++) {
                 ID_ADD.add(i);
                 ID_SUB.add(i);
                 ID_MUL.add(i);
@@ -88,43 +89,15 @@ public class QuestionUtil {
     }
 
     public static ArrayList<Integer> getIds(String op) {
-        if (op.equals(MainActivity.EXTRA_ADD))
+        if (op.equals(OperationArgs.EXTRA_ADD))
             return ID_ADD;
-        if (op.equals(MainActivity.EXTRA_SUB))
+        if (op.equals(OperationArgs.EXTRA_SUB))
             return ID_SUB;
-        if (op.equals(MainActivity.EXTRA_MUL))
+        if (op.equals(OperationArgs.EXTRA_MUL))
             return ID_MUL;
-        if (op.equals(MainActivity.EXTRA_DIV))
+        if (op.equals(OperationArgs.EXTRA_DIV))
             return ID_DIV;
         return null;
-    }
-
-    public static String getQuestion(int id, String op) {
-        return "Hello";
-    }
-
-    public static String getAnswer(int id, String op) {
-        return "Answer";
-    }
-
-    public static String[] getOptions(int id, String op) {
-        return new String[]{"a", "b", "c", "d"};
-    }
-
-    public static boolean getSolved(int id, String op) {
-        return true;
-    }
-
-    public static boolean setSolved(int id, String op) {
-        return true;
-    }
-
-    public static int getType(int id, String op) {
-        return 0;
-    }
-
-    public static void clearSolved(int id, String op) {
-
     }
 
 

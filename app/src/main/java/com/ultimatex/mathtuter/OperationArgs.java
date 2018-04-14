66d09656b3ac29a59ee761460b1c2eeeef0dccd0
lakinduakash <lakinduakash@gmail.com>
2018-04-15 02:46:37 +0530

    first commit

---
+++ app/src/main/java/com/ultimatex/mathtuter/OperationArgs.java
@@ -0,0 +1,10 @@
+package com.ultimatex.mathtuter;
+
+public interface OperationArgs {
+    public static final String EXTRA_KEY_OP = "com.ultimatex.mathtutor.operator";
+
+    public static final String EXTRA_ADD = "ADD";
+    public static final String EXTRA_SUB = "SUB";
+    public static final String EXTRA_MUL = "MUL";
+    public static final String EXTRA_DIV = "DIV";
+}

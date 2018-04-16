1d0646ec5a6c7221ca4598a4e9477d4183abaec4
lakinduakash <lakinduakash@gmail.com>
2018-04-17 02:30:43 +0530

    first commit

---
+++ app/src/main/java/com/ultimatex/mathtuter/MyApplication.java
@@ -0,0 +1,9 @@
+package com.ultimatex.mathtuter;
+
+import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
+
+public class MyApplication extends Application {
+    public SQLiteDatabase globalWritableDb;
+
+}

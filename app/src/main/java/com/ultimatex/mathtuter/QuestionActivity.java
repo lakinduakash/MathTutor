98e555312136bceb090f9b3a69bcf4a6701dd664
lakinduakash <lakinduakash@gmail.com>
2018-04-17 02:31:30 +0530

    add db open

--- app/src/main/java/com/ultimatex/mathtuter/QuestionActivity.java
+++ app/src/main/java/com/ultimatex/mathtuter/QuestionActivity.java
@@ -1,15 +1,20 @@
 package com.ultimatex.mathtuter;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
-public class QuestionActivity extends AppCompatActivity implements QuestionListFragment.OnFragmentInteractionListener {
+public class QuestionActivity extends AppCompatActivity implements QuestionListFragment.OnFragmentInteractionListener, QuestionFragment.DbObjectListener {
 
     String intentExtraOP;
     Intent intent;
+    volatile SQLiteDatabase db;
+
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +24,17 @@ public class QuestionActivity extends AppCompatActivity implements QuestionListF
         intent = getIntent();
         intentExtraOP = intent.getStringExtra(OperationArgs.EXTRA_KEY_OP);
 
+        new DbOpener().execute();
+
         FragmentManager fragmentManager;
-        FragmentTransaction fragmentTransaction;
+
 
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
 
         QuestionListFragment questionListFragment = new QuestionListFragment();
 
-        fragmentTransaction.add(R.id.fragment_container_question, questionListFragment).commit();
+        fragmentTransaction.add(R.id.fragment_container_question, questionListFragment);
 
 
     }
@@ -49,4 +56,31 @@ public class QuestionActivity extends AppCompatActivity implements QuestionListF
     public void onFragmentInteraction(int position) {
         addQuestionFragment(position);
     }
+
+    @Override
+    public SQLiteDatabase getDatabase() {
+        return db;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    private class DbOpener extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            db = new com.ultimatex.mathtuter.util.QuestionUtilSQLiteHelper(QuestionActivity.this).getWritableDatabase();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            fragmentTransaction.commit();
+
+        }
+    }
+
 }

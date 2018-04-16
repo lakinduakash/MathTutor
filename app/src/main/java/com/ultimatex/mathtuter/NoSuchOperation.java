0e42760432f34d07a6289c03bcbf3b013cb44c7b
lakinduakash <lakinduakash@gmail.com>
2018-04-17 02:31:00 +0530

    first commit

---
+++ app/src/main/java/com/ultimatex/mathtuter/NoSuchOperation.java
@@ -0,0 +1,9 @@
+package com.ultimatex.mathtuter;
+
+public class NoSuchOperation extends Exception {
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return super.fillInStackTrace();
+    }
+}

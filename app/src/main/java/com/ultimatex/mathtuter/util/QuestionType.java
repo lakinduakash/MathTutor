93d9e2323fbb2052dd625edcfb292fec08e10f60
lakinduakash <lakinduakash@gmail.com>
2018-04-19 15:57:30 +0530

    add question type interface

---
+++ app/src/main/java/com/ultimatex/mathtuter/util/QuestionType.java
@@ -0,0 +1,9 @@
+package com.ultimatex.mathtuter.util;
+
+public interface QuestionType {
+
+    public static final String MCQ = "m";
+    public static final String TEXT = "t";
+    public static final String IMAGE = "i";
+    public static final String IMAGE_TEXT = "it";
+}

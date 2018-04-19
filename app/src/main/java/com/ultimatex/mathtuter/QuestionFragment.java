dd6b18843e5a894ad85bf7c5ae6ce066a6df1e5a
lakinduakash <lakinduakash@gmail.com>
2018-04-19 16:01:57 +0530

    several updates

--- app/src/main/java/com/ultimatex/mathtuter/QuestionFragment.java
+++ app/src/main/java/com/ultimatex/mathtuter/QuestionFragment.java
@@ -1,26 +1,57 @@
 package com.ultimatex.mathtuter;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.ultimatex.mathtuter.util.DataDownloader;
+import com.ultimatex.mathtuter.util.QuestionType;
 import com.ultimatex.mathtuter.util.QuestionUtilDbOperation;
 
+import java.util.ArrayList;
+
 public class QuestionFragment extends Fragment {
+
+    static int ID = 0;
+    static int COLUMN_NAME_QUESTION = 1;
+    static int COLUMN_NAME_TYPE = 2;
+    static int COLUMN_NAME_OP1 = 3;
+    static int COLUMN_NAME_OP2 = 4;
+    static int COLUMN_NAME_OP3 = 5;
+    static int COLUMN_NAME_OP4 = 6;
+    static int COLUMN_NAME_ANSWER = 7;
+    static int COLUMN_NAME_SOLVED = 8;
+
 
     private static final String ARG_OP = "param1";
     private static final String ARG_ID = "param2";
 
     private String op;
     private int id;
-
-    SQLiteDatabase db;
+    ArrayList<String> questionArray;
+    ImageView imgView;
+    TextView questionText;
     private DbObjectListener mListener;
+    EditText editText;
+    private SQLiteDatabase db;
+    private QuestionUtilDbOperation dbOperation;
 
     public QuestionFragment() {
         // Required empty public constructor
@@ -58,12 +89,20 @@ public class QuestionFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        dbOperation = new QuestionUtilDbOperation(db, op);
+        questionArray = dbOperation.getQuestion(id);
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_question, container, false);
-        QuestionUtilDbOperation dbOperation = new QuestionUtilDbOperation(db, op);
-        ((TextView) view.findViewById(R.id.textView_question)).setText(dbOperation.getQuestion(id).get(7) + "whoo");
+
+
+        String head = "Question " + " ";
+        ((TextView) view.findViewById(R.id.textView_question)).setText(head);
+
+        createQuestion(view);
 
         return view;
     }
@@ -77,6 +116,94 @@ public class QuestionFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+
+    private void createQuestion(View view) {
+        imgView = view.findViewById(R.id.image_question);
+        questionText = view.findViewById(R.id.question_text);
+        editText = view.findViewById(R.id.editText_answer);
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    onSubmitAnswer(v.getText().toString());
+                }
+                onSubmitAnswer(v.getText().toString());
+                return true;
+            }
+        });
+
+
+        String type = questionArray.get(COLUMN_NAME_TYPE);
+
+        if (QuestionType.IMAGE.equals(type)) {
+
+            Activity activity = getActivity();
+
+            String filename = dbOperation.getTable() + "_" + id + DataDownloader.EXTENSION;
+            if (activity != null) {
+                Bitmap bitmap = BitmapFactory.decodeFile(activity.getApplicationContext().getFilesDir().getPath() + "/" + filename);
+                imgView.setVisibility(View.VISIBLE);
+                imgView.setImageBitmap(bitmap);
+            }
+        } else if (QuestionType.TEXT.equals(type)) {
+            imgView.setVisibility(View.GONE);
+            questionText.setVisibility(View.VISIBLE);
+            questionText.setText(questionArray.get(COLUMN_NAME_QUESTION));
+        } else if (QuestionType.IMAGE_TEXT.equals(type)) {
+            imgView.setVisibility(View.VISIBLE);
+            questionText.setVisibility(View.VISIBLE);
+            questionText.setText(questionArray.get(COLUMN_NAME_QUESTION));
+        } else if (QuestionType.MCQ.equals(type)) {
+
+        }
+    }
+
+    private void onSubmitAnswer(String answer) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog dialog = null;
+
+        if (questionArray.get(COLUMN_NAME_ANSWER).equals(answer)) {
+
+            Activity activity = getActivity();
+            View view = null;
+
+            if (activity != null) {
+                view = activity.getCurrentFocus();
+            }
+            if (view != null) {
+                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null)
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+
+            dbOperation.updateSolved(id, "true");
+            builder.setMessage("Your answer is correct")
+                    .setTitle("Congratulation").setPositiveButton("Go to Questions", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    FragmentManager fm = getFragmentManager();
+                    if (fm != null)
+                        fm.popBackStackImmediate();
+                }
+            });
+            dialog = builder.create();
+        } else {
+            builder.setMessage("Your answer is wrong")
+                    .setTitle("Try again!").setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            dialog = builder.create();
+        }
+
+        dialog.show();
     }
 
     public interface DbObjectListener {

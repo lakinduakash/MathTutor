3be6379cd8af82d73d457afc1a5dc6fe422d33d9
lakinduakash <lakinduakash@gmail.com>
2018-04-21 14:10:40 +0530

    add card view

--- app/src/main/java/com/ultimatex/mathtuter/util/QuestionListAdapter.java
+++ app/src/main/java/com/ultimatex/mathtuter/util/QuestionListAdapter.java
@@ -1,10 +1,11 @@
 package com.ultimatex.mathtuter.util;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ultimatex.mathtuter.R;
@@ -22,18 +23,16 @@ public class QuestionListAdapter extends RecyclerView.Adapter<QuestionListAdapte
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LinearLayout ll = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(
+        View v = LayoutInflater.from(parent.getContext()).inflate(
                 R.layout.question_list_item_view, parent, false
         );
-        return new ViewHolder(ll);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String title = "Question " + (position + 1);
-        LinearLayout ll = holder.ll;
-        TextView tv = ll.findViewById(R.id.textView_question_list);
-        tv.setText(title);
+        holder.textView.setText(title);
     }
 
     @Override
@@ -42,11 +41,13 @@ public class QuestionListAdapter extends RecyclerView.Adapter<QuestionListAdapte
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        LinearLayout ll;
+        CardView cv;
+        TextView textView;
 
-        public ViewHolder(LinearLayout ll) {
-            super(ll);
-            this.ll = ll;
+        public ViewHolder(View view) {
+            super(view);
+            this.cv = view.findViewById(R.id.card_view);
+            this.textView = view.findViewById(R.id.textView_question_list);
         }
 
     }
